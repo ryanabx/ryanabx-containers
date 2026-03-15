a7f4b1487c51bbe99fc3b1767ab61e78d5c55922
Ryan Brue <ryanbrue.dev@gmail.com>
2026-03-15 13:29:08 -0500

    Attempt to trigger rebuild

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -2,6 +2,7 @@
 
 # Install common packages across ryanabx-dev and ryanabx-dev-ostree
 
+
 # Import vscode to the container
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
