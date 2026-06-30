ab32638c33725db84994943c0830019443e44ce9
Ryan Brue <ryanbrue.dev@gmail.com>
2026-06-29 23:00:45 -0500

    Add ryanabx-bootc

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,37 +1,26 @@
 #!/bin/sh
 
-# Install common packages across ryanabx-dev and ryanabx-dev-ostree
-
-
-# Import vscode to the container
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-# Install packages
+# Install common packages across ryanabx-dev and ryanabx-bootc
 dnf install -y \
-    zsh \
     helix \
-    wget \
-    code \
-    breeze-icon-theme breeze-cursor-theme google-noto-sans-fonts \
     git git-lfs \
-    gh pagure-cli \
+    wget \
     fedpkg fedora-packager \
+    gh pagure-cli \
     rust cargo rust-analyzer rustfmt clippy \
     gcc gcc-c++ clang clang-devel \
     make automake cmake just \
     android-tools pmbootstrap \
-    ruby ruby-devel \
+    nodejs npm \
+    dotnet-sdk-10.0 \
     perl-core \
-    nodejs \
-    dotnet-sdk-9.0 \
     wayland-devel libxkbcommon-devel fuse-devel libinput-devel \
     squashfs-tools gstreamer1-plugins-base-devel \
     gdk-pixbuf2-devel pango-devel gtk3-devel glib2-devel \
     lld mold kernel-devel glibc-devel binutils binutils-devel binutils-gold \
     libsoup-devel webkit2gtk4.1-devel \
     libseat-devel pixman-devel flatpak-devel \
-    osbuild-selinux mkosi \
-    kwallet \
+    mkosi \
     rsync
 # Finally, after installing, clean all
 dnf clean all
